25f4d42a7bee3a7a638b6f3e9d7bb11f70743b1f
chris_RuiWang <chriswangrui@github.com>
2020-06-01 22:35:26 -0700

    set up new project for C

---
+++ Practise/cmake-build-debug/CMakeFiles/Practise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practise.dir/main.c.o"
+  "Practise"
+  "Practise.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Practise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
